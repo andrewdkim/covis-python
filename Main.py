777e76f787aca4cd4b5ecd14a94a2f1773992e7f
andrewdkim <adkim414@gmail.com>
2022-07-23 12:29:27 -0400

    switched cateogry labels

--- Main.py
+++ Main.py
@@ -2,6 +2,10 @@ import numpy as np
 from Explicit import *
 
 
+
+
+
+
 def open_data(filepath: str):
   with open(filepath, "r") as file:
     data = []
@@ -12,16 +16,16 @@ def open_data(filepath: str):
 def rules():
   def spatial(entry):
     [_, spatial, _] = entry
-    return True if spatial > -20 else False
+    return 1 if spatial > -20 else 2
 
   def spatial_and_orientation(entry):
     [_, spatial, ori] = entry
-    return True if spatial > -20 and ori > 11.25 else False
+    return 1 if spatial > -20 and ori > 11.25 else 2
 
   
   def orientation(entry):
     [_, _, ori] = entry
-    return True if ori > 11.25 else False
+    return 1 if ori > 11.25 else 2
 
   return [spatial, spatial_and_orientation, orientation]
 
@@ -30,8 +34,6 @@ def main():
   data = open_data("data/RB_triangle_v1.txt")
   explicit = Explicit(data, rules())
   explicit.run_trials()
-  
-
 
 if __name__ == "__main__":
   main()
