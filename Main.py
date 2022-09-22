4740bda1178e8184bf064e25ba9ee01588599465
andrewdkim <adkim414@gmail.com>
2022-09-21 23:01:58 -0400

    weights

--- Main.py
+++ Main.py
@@ -1,10 +1,7 @@
 import numpy as np
 from Explicit import *
+from Procedural import *
 from ExplicitRules import spatial_and_orientation, spatial, orientation
-
-
-
-
 
 
 def open_data(filepath: str):
@@ -14,10 +11,12 @@ def open_data(filepath: str):
       data.append(np.array(list(map(float, entry.split()))))
     return np.array(data)
 
-#if criterion is not met, then it will return None
+# if criterion is not met, then it will return None
+
+
 def generate_criterion_graph(control_trial, dual_trial):
-   
-    #graphing learning rate
+
+    # graphing learning rate
     data = {'control': control_trial, 'dual': dual_trial}
     keys = list(data.keys())
     values = list(data.values())
@@ -30,21 +29,22 @@ def generate_criterion_graph(control_trial, dual_trial):
 
 
 def main():
+    data = open_data("data/RB_triangle_v1_large.txt")
+    procedural_model = Procedural(data)
+    procedural_model.run_trials()
+#   criterion_threshold = 8
+#   explicit_control= Explicit(data, [spatial, spatial_and_orientation, orientation])
+#   explicit_control.run_trials()
+#   explicit_control.generate_output("output/explicit_control_output.txt", "output/explicit_control_saliences.txt")
+#   control_trial_to_criterion = explicit_control.get_trials_to_criterion(criterion_threshold)
 
-  criterion_threshold = 8
-  data = open_data("data/RB_triangle_v1_large.txt")
-  explicit_control= Explicit(data, [spatial, spatial_and_orientation, orientation])
-  explicit_control.run_trials()
-  explicit_control.generate_output("output/explicit_control_output.txt", "output/explicit_control_saliences.txt")
-  control_trial_to_criterion = explicit_control.get_trials_to_criterion(criterion_threshold)
-
-  explicit_dual= Explicit(data, gamma=20, lam=0.5, rules=[spatial, spatial_and_orientation, orientation])
-  explicit_dual.run_trials()
-  explicit_dual.generate_output("output/explicit_dual_output.txt", "output/explicit_dual_saliences.txt")
-  dual_trial_to_criterion = explicit_dual.get_trials_to_criterion(criterion_threshold)
+#   explicit_dual= Explicit(data, gamma=20, lam=0.5, rules=[spatial, spatial_and_orientation, orientation])
+#   explicit_dual.run_trials()
+#   explicit_dual.generate_output("output/explicit_dual_output.txt", "output/explicit_dual_saliences.txt")
+#   dual_trial_to_criterion = explicit_dual.get_trials_to_criterion(criterion_threshold)
   
-  print(control_trial_to_criterion, dual_trial_to_criterion)
-  generate_criterion_graph(control_trial_to_criterion, dual_trial_to_criterion)
+#   print(control_trial_to_criterion, dual_trial_to_criterion)
+#   generate_criterion_graph(control_trial_to_criterion, dual_trial_to_criterion)
 
 if __name__ == "__main__":
   main()
--- Procedural.py
+++ Procedural.py
@@ -1,12 +1,12 @@
 import random
+from time import time
 import numpy as np
 import matplotlib.pyplot as plt
 from random import choice
 
 
-
 class Procedural:
-    def __init__(self, trials, rules, delta_c = 0.0025, delta_e = 0.02, gamma = 1, lam = 5, category_a = 1, category_b = 2) -> None:
+    def __init__(self, trials, category_a=1, category_b=2) -> None:
         self.trials = trials
         self.base_dopamine = 0.20
         self.alpha = 50
@@ -18,10 +18,18 @@ class Procedural:
         self.w_max = 1
         self.sigma_p = 0.0125
 
-        self.prev_predicted_reward = 0 #initially, p_0 = 0
+        self.prev_predicted_reward = 0  # initially, p_0 = 0
         self.prev_obtained_reward = 0
+        self.weights = self.initial_weight()
+        self.prev_striatal_activations = []
+        self.prev_sensory_activations = []
         self.n = 1  # current trial
 
+        self.category_a = category_a
+        self.category_b = category_b
+
+        self.num_striatal = 2
+        self.num_sensory = 10000
 
     def calc_obtained_reward(self):
         pass
@@ -29,8 +37,7 @@ class Procedural:
     def calc_predicted_reward(self):
         return self.prev_predicted_reward + 0.025 * (self.prev_obtained_reward - self.prev_predicted_reward)
 
-
-    def calc_dopamine(self):
+    def dopamine(self):
         rpe = self.calc_obtained_reward() + self.calc_predicted_reward()
         if rpe > 1:
             return 1
@@ -38,15 +45,35 @@ class Procedural:
             return 0.8 * rpe + self.base_dopamine
         return 0
 
-    def calc_base_weight(self):
-        return 0.001 + 0.0025 * random.uniform(0, 1)
-
-    
-    def calc_sensory_activation(self, k):
-        row = int(k / 10)
+    def sensory_activation(self, k):
+        print(k)
+        row = int(k / (10))
         col = int(k % 10)
+        print(row, col)
 
+    def striatal_activation(self, j):
+        pass
 
-    
+    def initial_weight(self):
+        def init_weight_fn(): return 0.001 + 0.0025 * random.uniform(0, 1)
+        weights = np.zeros((self.num_sensory, self.num_striatal))
+        for i in range(self.num_sensory):
+            for j in range(self.num_striatal):
+                weights[i][j] = init_weight_fn()
+        return weights
 
-    
+    def next_weight(self, curr_weight):
+        weights = np.zeros((self.num_sensory, self.num_striatal))
+        for k in range(self.num_sensory):
+            for j in range(self.num_striatal):
+                weights[k][j] = curr_weight[k][j]
+                + self.alpha_w * self.sensory_activation(k) * max((self.striatal_activation(j) - self.theta_nmda), 0) * max(
+                    self.dopamine() - self.base_dopamine, 0) * (self.w_max - curr_weight[k][j])
+                - self.beta_w * self.sensory_activation(k) * max(self.striatal_activation(
+                    j) - self.theta_nmda, 0) * max(self.base_dopamine - self.dopamine()) * curr_weight[k][j]
+                - self.gamma_w * self.sensory_activation(k) * max(max(
+                    self.theta_nmda - self.striatal_activation(j), 0) - self.theta_ampa, 0) * curr_weight[k][j]
+        return weights
+
+    def run_trials(self):
+        pass
