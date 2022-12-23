d458028c2c0a38a1fdbc23d1d39591b714da4256
andrewdkim <adkim414@gmail.com>
2022-12-22 21:39:29 -0500

    increase explicit epsilon to -15 to 15

--- ExplicitRules.py
+++ ExplicitRules.py
@@ -1,20 +1,20 @@
 import random
 def spatial(entry, category_a=1, category_b=2):
     [_, spatial, _] = entry
-    epsilon = random.randint(-100, 100)
+    epsilon = random.randint(-15, 15)
     criterion = 22.5
     return category_a if (spatial - criterion) > epsilon else category_b
 
 
 def spatial_and_orientation(entry, category_a=1, category_b=2):
     [_, spatial, ori] = entry
-    epsilon = random.randint(-100, 100)
+    epsilon = random.randint(-15, 15)
     criterion = 22.5
     return category_a if ((spatial - criterion) > epsilon) and ((ori - criterion) > epsilon) else category_b
 
 
 def orientation(entry, category_a=1, category_b=2):
     [_, _, ori] = entry
-    epsilon = random.randint(-100, 100)
+    epsilon = random.randint(-15, 15)
     criterion = 22.5
     return category_a if (ori - criterion) > epsilon else category_b
